8a99bc99bd2ce4319966da1e9c79620e2d079249
hrideshrai <hridershrai0001@gmail.com>
2024-12-12 08:22:21 +0545

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace Hridesh_rai.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Student> Students { get; set; }
+
+}
---
+++ Repositories/GenericRepository/Repository.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.Repositories.GenericRepository
+{
+    public class Repository
+    {
+    }
+}
